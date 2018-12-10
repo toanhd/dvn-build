89cf1b23697092f2d2674a0280feb8ff582c843e
ToanHD <toanhanduc@gmail.com>
2018-12-10 18:30:19 +0700

    deploy logic

--- composer-dvn/lib/logic.js
+++ composer-dvn/lib/logic.js
@@ -19,25 +19,103 @@
 
 /**
  * Sample transaction
- * @param {org.dvn.com.SampleTransaction} sampleTransaction
+ * @param {org.dvn.com.Grading} Grading
  * @transaction
  */
-async function sampleTransaction(tx) {
-    // Save the old value of the asset.
-    const oldValue = tx.asset.value;
+async function Grading(GradingTx) {
+    //Action type
+    let action = '';
+    //create format for transcript id
+    let transcriptID = "transcript_" + GradingTx.stdMemberID;
+    //Asset Registry for object type Transcript
+    let transcriptReg = await getAssetRegistry('org.dvn.com.Transcript');
+    //check existence of transcript
+    let availTranscript = await transcriptReg.exists(transcriptID);
 
-    // Update the asset with the new value.
-    tx.asset.value = tx.newValue;
+    //Asset Registry for object type Student
+    let studentReg = await getParticipantRegistry('org.dvn.com.Student');
+    //check existence of student
+    let availStd = await studentReg.exists(GradingTx.stdMemberID);
 
-    // Get the asset registry for the asset.
-    const assetRegistry = await getAssetRegistry('org.dvn.com.SampleAsset');
-    // Update the asset in the asset registry.
-    await assetRegistry.update(tx.asset);
+    //Asset Registry for object type Lecturer
+    let lecReg = await getParticipantRegistry('org.dvn.com.Lecturer');
+    //check existence of student
+    let availLec = await lecReg.exists(GradingTx.lecMemberID);
 
-    // Emit an event for the modified asset.
-    let event = getFactory().newEvent('org.dvn.com', 'SampleEvent');
-    event.asset = tx.asset;
-    event.oldValue = oldValue;
-    event.newValue = tx.newValue;
-    emit(event);
+    //Get Factory to create new resource
+    let factory = getFactory();
+
+    //Asset Registry for object type Grade
+    let gradeReg = await getAssetRegistry('org.dvn.com.Grade');
+
+    if (availStd && availLec) {
+        if (availTranscript) {
+            var transcript = await transcriptReg.get(transcriptID);
+
+            //Create new Grade
+            let gradeID = transcriptID + "grade_" + (transcript.gradesList.length + 1);
+            let newGrade = factory.newResource('org.dvn.com', 'Grade', gradeID.toString());
+            newGrade.issueDate = GradingTx.timestamp;
+            newGrade.courseID = GradingTx.courseID;
+            newGrade.courseName = GradingTx.courseName;
+            newGrade.credit = GradingTx.credit;
+            newGrade.gradeVal = GradingTx.gradeVal;
+            newGrade.semester = GradingTx.semester;
+
+            //Create relationship
+            newGrade.lecturer = factory.newRelationship('org.dvn.com', 'Lecturer', GradingTx.lecMemberID);
+            //newGrade.student = factory.newRelationship('org.dvn.com', 'Student', GradingTx.stdMemberID);
+
+            //Update grade to grade registry
+            await gradeReg.add(newGrade);
+
+            //Save new grade to transcript
+            transcript.gradesList.push(newGrade);
+
+            //Update transcript to transcript registry
+            await transcriptReg.update(transcript);
+
+        } else { //this student transcript is not exist
+            //Create new Transcript
+            let newTranscript = factory.newResource('org.dvn.com', 'Transcript', transcriptID);
+            //Tao relationship cho doi tuong reference
+            newTranscript.student = factory.newRelationship('org.dvn.com', 'Student', GradingTx.stdMemberID);
+            newTranscript.gradesList = [];
+
+            //Create new Grade
+            let gradeID = transcriptID + "grade_" + (newTranscript.gradesList.length + 1);
+            let newGrade = factory.newResource('org.dvn.com', 'Grade', gradeID.toString());
+            newGrade.issueDate = GradingTx.timestamp;
+            newGrade.courseID = GradingTx.courseID;
+            newGrade.courseName = GradingTx.courseName;
+            newGrade.credit = GradingTx.credit;
+            newGrade.gradeVal = GradingTx.gradeVal;
+            newGrade.semester = GradingTx.semester;
+
+            //Create relationship
+            newGrade.lecturer = factory.newRelationship('org.dvn.com', 'Lecturer', GradingTx.lecMemberID);
+            //newGrade.student = factory.newRelationship('org.dvn.com', 'Student', GradingTx.stdMemberID);
+
+            //Update grade to grade registry
+            await gradeReg.add(newGrade);
+
+            //Save new grade to transcript
+            newTranscript.gradesList.push(newGrade);
+
+            //add transcript to transcript registry
+            await transcriptReg.add(newTranscript);
+
+        }
+        //Create new event
+        let GradingEvent = factory.newEvent('org.dvn.com', 'GradingEvent');
+        GradingEvent.gradingTransaction = GradingTx;
+        GradingEvent.action = action;
+        
+        //Emit event
+        emit(GradingEvent);
+    } else {
+       throw new ReferenceError (
+           "Student or Lecture not found"
+       )
+    }
 }
